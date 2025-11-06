22613e6b06fc2f5e8b64898f5be0bd209c843657
TaeWilliam <williamhaenschke@gmail.com>
2025-11-06 14:44:52 +0100

    PlayerController
    Added PlayerController script, and made a basic character, that can move left and right. Currently using input system, as copilot autocompleted the code and ppl came to our group which was distracting. Gonna make it work properly at home I think.
    Can't move up and down because I've never used input system. I'll check it out at home.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float walkSpeed = 5f;
+    public float crouchSpeed = 2f;
+    public bool isCrouching = false;
+    public bool isGrounded = true;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // check wasd input, and move character accordingly
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (isCrouching)
+        {
+            transform.Translate(movement * crouchSpeed * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            transform.Translate(movement * walkSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
