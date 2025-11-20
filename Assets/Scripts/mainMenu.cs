f407bdcb1267be56d2e2b6930f966dbc12c2eb79
Sebastian <sebastianleosorensen@gmail.com>
2025-11-20 14:27:37 +0100

    main menu script fra lucas' unity

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    
+public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("newSceneLoaded");
+
+    }
+public void QuitGame ()
+    {
+        Application.Quit();
+       
+        Debug.Log("QUIT");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
