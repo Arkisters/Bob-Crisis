4d215a5f22e75db8b218fe90f2ed530354d65673
fj1014 <159522634+fj1014@users.noreply.github.com>
2025-11-21 14:29:27 +0100

    assest smog script

---
+++ Assets/Scripts/TarSurface.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TarSurface : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //change variable in the player controller to true
+            //collision.GetComponent<PlayerController>().isMovingInTar = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            //change variable in the player controller to false
+            //collision.GetComponent<PlayerController>().isMovingInTar = true;
+        }
+    }
+}
