3efe430c811d880aebf4df0ea083bf2f0a5c9b29
fj1014 <159522634+fj1014@users.noreply.github.com>
2025-11-27 13:22:00 +0100

    Revert "Merge branch 'Harman'"
    This reverts commit a890c1dc43f3a82304bd2db6dc1b4e19e7c1d5f9, reversing
    changes made to 66eb91e60be383afa8624cc18d7d42fd11f509d3.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEditor.Rendering.Analytics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,21 +8,11 @@ public class Health : MonoBehaviour
     public float maxHealth = 5;
     public int deathTimer = 3;
     public bool takingDamage = false;
-    Vector2 checkpointPos;
-
-    private SpriteRenderer Player_0;
-
-    private void Awake()
-    {
-        Player_0 = GetComponent<SpriteRenderer>();
-    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        health = maxHealth;
-
-        checkpointPos = transform.position; 
+        health = maxHealth; 
 
     }
 
@@ -36,50 +25,16 @@ public class Health : MonoBehaviour
         }
         if (health <= 0)
         {
-            Die();
+            Destroy(gameObject);
+            SceneManager.LoadScene("LeyoBareLeyo");
         }
     }
 
-    public void Updatecheckpoint(Vector2 pos)
-    {
-        checkpointPos = pos;
-    }
-    
-            
 
-    void Die()
-    {
-        StartCoroutine(Respawn(0.5f));
-    }
-
-    IEnumerator Respawn(float duration)
-    {
-        Player_0.enabled = false;
-        yield return new WaitForSeconds(duration);
-        transform.position = checkpointPos;
-        health = maxHealth;
-        Player_0.enabled = true;
-    }
-
-    void Respawn()
-    {
-        transform.position = checkpointPos;
-
-        health = maxHealth;
-    }
 
     public void TakeDamage(float amount)
     {
         health -= amount;
         takingDamage = true;
-        StartCoroutine(Pain(1.0f));
-    }
-    IEnumerator Pain(float duration)
-    {    
-        Player_0.color = Color.red;
-        yield return new WaitForSeconds (.5f);
-        yield return null;
-        Player_0.color = Color.white;
-        yield return null;
     }
 }
