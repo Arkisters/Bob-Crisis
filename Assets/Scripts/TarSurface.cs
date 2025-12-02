a4fd1267a6df90c2ddbaeb82a367d6d663504613
Sebastian <sebastianleosorensen@gmail.com>
2025-12-02 13:59:36 +0100

    i forgot

--- Assets/Scripts/TarSurface.cs
+++ Assets/Scripts/TarSurface.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class TarSurface : MonoBehaviour
 {
+
+    public bool isMovingInTar = false;
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.CompareTag("Player"))
         {
             //change variable in the player controller to true
