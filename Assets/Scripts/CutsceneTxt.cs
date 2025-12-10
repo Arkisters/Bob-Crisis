0a2ea71789145d69fb7afd8f46d1f676b3257064
Sebastian <sebastianleosorensen@gmail.com>
2025-12-10 11:51:44 +0100

    cutscene wip

---
+++ Assets/Scripts/CutsceneTxt.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CutsceneTxt : MonoBehaviour
+{
+
+    public int wordSpeed;
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+    public string[] dialogue;
+    private int index = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
