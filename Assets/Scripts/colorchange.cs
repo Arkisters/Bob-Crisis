90d29080bcefd808d7cdd7147ced40e544c2573e
Tae <taehaenschke@gmail.com>
2025-12-12 05:37:48 +0100

    Some leyoscene changes and the tilemapswapper being automatic and gradual yeah

--- Assets/Scripts/colorchange.cs
+++ Assets/Scripts/colorchange.cs
@@ -4,28 +4,38 @@ using System.Collections;
 public class colorchange : MonoBehaviour
 {
     [SerializeField] private VolumeMode volumeMode = VolumeMode.Both;
+    [SerializeField] private TilemapSwapper tilemapSwapper;
+    
+    private bool hasTriggered = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !hasTriggered)
         {
+            hasTriggered = true;
+            
+            // Trigger post processing volume change
             PostProcessingEnabler enabler = Camera.main.GetComponent<PostProcessingEnabler>();
-            if (enabler == null)
+            if (enabler != null)
             {
-                return;
+                enabler.SetVolumeMode(volumeMode);
+                enabler.SwitchVolumes(0.5f);
             }
-            enabler.SetVolumeMode(volumeMode);
-            enabler.SwitchVolumes(0.5f);
+            
+            // Trigger tilemap freshness increase
+            if (tilemapSwapper != null)
+            {
+                tilemapSwapper.IncreaseFreshness();
+            }
+            else
+            {
+                Debug.LogWarning("colorchange: TilemapSwapper reference not assigned!");
+            }
         }
-
-
     }
 
- 
-
-
-// Update is called once per frame
-void Update()
+    // Update is called once per frame
+    void Update()
     {
         
     }
